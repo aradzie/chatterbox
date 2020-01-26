66ba895bafe23ca48f94be8349367f4430fad87b
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2020-01-26 13:32:23 +0100

    Add reminders to fix tests

--- src/util/text/text.test.ts
+++ src/util/text/text.test.ts
@@ -3,8 +3,14 @@ import { grammars } from "../../grammar";
 import { parse } from "./parse";
 import { print } from "./print";
 
-for (const { name, grammar } of [grammars[0]]) {
-  test(`print and parse [${name}]`, (t) => {
+test(`print and parse example grammar`, (t) => {
+  const { grammar } = grammars[0];
+  const copy = parse(print(grammar));
+  t.deepEqual(copy, grammar);
+});
+
+for (const { name, grammar } of grammars) {
+  test.skip(`print and parse grammar [${name}]`, (t) => {
     const copy = parse(print(grammar));
     t.deepEqual(copy, grammar);
   });
