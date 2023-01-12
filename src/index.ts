3d795ac7c4236d85f66a785973a6592ebf7a38a4
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2023-01-12 13:49:59 +0100

    Rename files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./ast.js";
 export * from "./generate.js";
+export * from "./grammar/index.js";
 export * from "./merge.js";
 export * from "./optimize.js";
-export * from "./text/index.js";
 export * from "./validate.js";
